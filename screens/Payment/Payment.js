afa971a96505ca511bb24d7a9b1635ca993fe21c
miyoung <noonchicat@gmail.com>
2025-04-08 16:47:58 +0900

    create constants.js and ignore git

--- screens/Payment/Payment.js
+++ screens/Payment/Payment.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {useSelector} from 'react-redux';
 import {SafeAreaView, ScrollView, Text, View} from 'react-native';
+import {CardForm, StripeProvider} from '@stripe/stripe-react-native';
 import Header from '../../components/Header/Header';
 
 import globalStyle from '../../assets/styles/globalStyle';
@@ -20,6 +21,11 @@ const Payment = () => {
           <Text style={style.donationAmountDescription}>
             You are about to donate ${donationItemInformation.price}
           </Text>
+        </View>
+        <View>
+          <StripeProvider publishableKey={STRIPE_PUBLISHABLE_KEY}>
+            <CardForm style={style.cardForm} />
+          </StripeProvider>
         </View>
       </ScrollView>
       <View style={style.button}>
